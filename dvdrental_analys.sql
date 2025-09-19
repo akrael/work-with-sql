dffac6d7ebd545b23bf3f40e5234a6e82e6e00ee
gabriel <65725138+akrael@users.noreply.github.com>
2025-09-19 14:34:38 +0300

    Update dvdrental_analys.sql

--- dvdrental_analys.sql
+++ dvdrental_analys.sql
@@ -1,77 +1,75 @@
---- ïîëó÷åíèå ïîëåé
+--- получение полей
 select
-    payment_id as "¹ ïëàòåæà",
-    customer_id as "¹ ïîêóïàòåëÿ",
-    amount as "ñóììà ïëàòåæà"
+    payment_id as "№ платежа",
+    customer_id as "№ покупателя",
+    amount as "сумма платежа"
 from
-    payment
+    payment
 
---- âûâîä ñóììû ïëàòåæà â ðóáëÿõ 
+--- вывод суммы платежа в рублях 
 select
-    amount as "ñóììà  äîëëàðàõ" ,
-    amount * 85 as "Ñóììà â ðóáëÿõ"
+    amount as "сумма  долларах" ,
+    amount * 85 as "Сумма в рублях"
 from
-    payment p
---- âûâîä óíèêàëüíûõ çíà÷åíèé 
+    payment p
+--- вывод уникальных значений 
 select
-    distinct rental_duration
+    distinct rental_duration
 from
-    film f
---- âûâîä èìåí àêòåðîâ ÷üè èìåíà íà÷èíàþòñÿ íà R
+    film f
+--- вывод имен актеров чьи имена начинаются на R
 select
-    first_name as "Èìåíà íà 'R'"
+    first_name as "Имена на 'R'"
 from
-    actor a
+    actor a
 where
-    first_name like 'R%'
---- ôèëüòðàöèÿ âûâîäà äàííûõ 
---- 1) ïî ñòîèìîñòè 
+    first_name like 'R%'
+--- фильтрация вывода данных 
+--- 1) по стоимости 
 select
-    amount
+    amount
 from
-    payment
+    payment
 where
-    amount > 7.00
-    or amount between 3.3 and 5.5
---- 2) ïî äëèííå ôèëüìà
+    amount > 7.00
+    or amount between 3.3 and 5.5
+--- 2) по длинне фильма
 select
-    title as "íàçâàíèå ôèëüìà",
-    length  as "ïðîäîëæèòåëüíîñòü ôèëüìà"
+    title as "название фильма",
+    length  as "продолжительность фильма"
 from
-    film f
+    film f
 where f.length = 87
 or f.length = 116
 or f.length = 184
---- Âûâîä ôèëüìîâ ïî âûáðàííîé êàòåãîðèè 
---- ðàáîòà ñ ïðîìåæóòî÷íîé òàáëèöåé
+--- Вывод фильмов по выбранной категории 
+--- работа с промежуточной таблицей
 select
-    f.title as "film name",
-    c."name" 
+    f.title as "film name",
+    c."name" 
 from
-    film_category fc
+    film_category fc
 join film f on fc.film_id = f.film_id
 join category c on fc.category_id = c.category_id 
 where c."name" = ('Comedy')
---- ðàáîòà ñ ïðîìåæóòî÷íîé òàáëèöåé, áåç ôèëüòðà
---- ïî êàòåãîðèè
+--- работа с промежуточной таблицей, без фильтра
+--- по категории
 select
-    f.title as "film name",
-    c."name" 
+    f.title as "film name",
+    c."name" 
 from
-    film_category fc
+    film_category fc
 join film f on fc.film_id = f.film_id
 join category c on fc.category_id = c.category_id 
 order by "film name" 
---- Ñîâìåùåíèå äàííûõ èç ðàçíûõ ñòîëáöîâ 
---- ðàáîòà ñ ïðîìåæóòî÷íîé òàáëèöåé äëÿ ïîëó÷åíèÿ äàííûõ
---- î àêòåðàõ ñ ôèëüìîãðàôèåé áîëüøå 20òè ôèëüìîâ 
+--- Совмещение данных из разных столбцов 
+--- работа с промежуточной таблицей для получения данных
+--- о актерах с фильмографией больше 20ти фильмов 
 select
-    a.first_name || ' ' || a.last_name as name,
-    count(film_id) as film_number
+    a.first_name || ' ' || a.last_name as name,
+    count(film_id) as film_number
 from
-    film_actor fa
+    film_actor fa
 join actor a on fa.actor_id = a.actor_id
 group by name 
 having count(film_id) > 20
-
-
